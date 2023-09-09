3c630f1fdfbcef25888e7b5f3455b8450f83656e
Auridh <git@auridh.net>
2023-09-09 18:34:06 +0200

    :bug: Fix database name

--- mod/Mods/AuridhDS/ScriptExtender/Lua/Game/FirstInstall.lua
+++ mod/Mods/AuridhDS/ScriptExtender/Lua/Game/FirstInstall.lua
@@ -1,12 +1,14 @@
 function DSFirstInstall()
     Log('DSFirstInstall')
 
-    PersistentState.ModState = {
-        Installed = true,
-    }
+    PersistentState.ModState.Installed = true
+    PersistentState.AddedToCampChests = {}
 
-    local campChests = Osi.DB_UserCampChest:Get(nil)
+    local campChests = Osi.DB_Camp_UserCampChest:Get(nil, nil)
     for _, v in pairs(campChests) do
-        Osi.TemplateAddTo('8ea7b456-364a-421d-abdc-2b7cd7ca5b21', v[2], 1, 1)
+        if PersistentState.AddedToCampChests[v[2]] == nil then
+            Osi.TemplateAddTo('8ea7b456-364a-421d-abdc-2b7cd7ca5b21', v[2], 1, 1)
+            PersistentState.AddedToCampChests[v[2]] = true
+        end
     end
 end
