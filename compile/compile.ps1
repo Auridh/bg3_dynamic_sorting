1db1c7dddf667127bae6fb17eb7ab27fccbf2613
Auridh <git@auridh.net>
2023-09-09 14:16:52 +0200

    :construction: Update compile script

--- compile/compile.ps1
+++ compile/compile.ps1
@@ -1,4 +1,8 @@
 param ($n)
+$modName = "DynamicSorting"
+$modHandle = "AuridhDS"
 $absPath = Resolve-Path -Path "./"
+divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/English/$modName.xml" -d "$absPath/mod/Localization/English/$modName.loca";
+divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/RootTemplates" -d "$absPath/mod/Public/$modHandle/RootTemplates" -i lsx -o lsf;
 divine -a create-package -g bg3 -s "$absPath/mod" -d "$absPath/compile/files/$n.pak" -c lz4;
 Compress-Archive -Path "$absPath/compile/files/$n.pak" -DestinationPath "$absPath/compile/files/$n.zip" -Force
