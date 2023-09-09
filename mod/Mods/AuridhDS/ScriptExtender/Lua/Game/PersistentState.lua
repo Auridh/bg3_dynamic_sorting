76f36b282a7f1847b650a691f7fc4a0c9bae3428
Auridh <git@auridh.net>
2023-09-09 18:27:36 +0200

    :bug: Do not log before PersistentState is set

--- mod/Mods/AuridhDS/ScriptExtender/Lua/Game/PersistentState.lua
+++ mod/Mods/AuridhDS/ScriptExtender/Lua/Game/PersistentState.lua
@@ -1,22 +1,22 @@
-PersistentState = {
-    ModState = {
-        Installed = false,
-        LoggingEnabled = true,
-    },
-}
+PersistentState = nil
 
-local StateFile = 'AuridhDS\\PersistentState.json'
+local StateFile = 'PersistentState'
 local GameState_Save = 'Save'
 
 Ext.Events.SessionLoaded:Subscribe(function()
+    PersistentState = Ext.IO.LoadFile(StateFile) or {
+        ModState = {
+            Installed = false,
+            LoggingEnabled = true,
+        },
+    }
     Log('SessionLoaded')
-    PersistentState = Ext.IO.LoadFile(StateFile)
 end)
 
 Ext.Events.GameStateChanged:Subscribe(function(e)
-    Log('GameStateChanged: ')
-    Dmp(e)
     if e.ToState == GameState_Save then
+        Log('GameStateChanged: ')
+        Dmp(e)
         Ext.IO.SaveFile(StateFile, Ext.Json.Stringify(PersistentState))
     end
 end)
