50b3c4f2d0c6fddf9f6b20b47e6f33b8381d4544
Auridh <git@auridh.net>
2023-09-11 19:58:39 +0200

    :art: Add new icons

--- compile/compile.ps1
+++ compile/compile.ps1
@@ -4,5 +4,7 @@ $modHandle = "AuridhDS"
 $absPath = Resolve-Path -Path "./"
 divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/English/$modName.xml" -d "$absPath/mod/Localization/English/$modName.loca";
 divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/RootTemplates" -d "$absPath/mod/Public/$modHandle/RootTemplates" -i lsx -o lsf;
+divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/Content/UI/[PAK]_UI" -d "$absPath/mod/Public/$modHandle/Content/UI/[PAK]_UI" -i lsx -o lsf;
+divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/GUI" -d "$absPath/mod/Public/$modHandle/GUI" -i lsx -o lsf;
 divine -a create-package -g bg3 -s "$absPath/mod" -d "$absPath/compile/files/$n.pak" -c lz4;
 Compress-Archive -Path "$absPath/compile/files/$n.pak" -DestinationPath "$absPath/compile/files/$n.zip" -Force
