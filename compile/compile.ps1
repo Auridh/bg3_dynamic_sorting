b0c73e493b7154c58cca64fdf400b31cb9c11555
Auridh <git@auridh.net>
2023-10-05 03:09:03 +0200

    :sparkles: Better compile script

--- compile/compile.ps1
+++ compile/compile.ps1
@@ -1,10 +1,26 @@
 param ($n)
-$modName = "DynamicSorting"
-$modHandle = "AuridhDS"
 $absPath = Resolve-Path -Path "./"
-divine -a convert-loca -g bg3 -s "$absPath/mod/Localization/English/$modName.xml" -d "$absPath/mod/Localization/English/$modName.loca";
-divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/RootTemplates" -d "$absPath/mod/Public/$modHandle/RootTemplates" -i lsx -o lsf;
-divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/Content/UI/[PAK]_UI" -d "$absPath/mod/Public/$modHandle/Content/UI/[PAK]_UI" -i lsx -o lsf;
-divine -a convert-resources -g bg3 -s "$absPath/mod/Public/$modHandle/GUI" -d "$absPath/mod/Public/$modHandle/GUI" -i lsx -o lsf;
-divine -a create-package -g bg3 -s "$absPath/mod" -d "$absPath/compile/files/$n.pak" -c lz4;
+
+# bump build version (Version.lua)
+(Get-Content "$absPath/mod/Mods/AuridhDS/ScriptExtender/Lua/Version.lua") `
+    -replace '(Build =) (\d*)',{ '{0}' -f $_.Groups[1].Value + ' ' + ([int]$_.Groups[2].Value + 1) } `
+    -replace '(\d*\.\d*\.\d*\.)(\d*)',{ '{0}' -f $_.Groups[1].Value + ([int]$_.Groups[2].Value + 1) } `
+| Set-Content "$absPath/mod/Mods/AuridhDS/ScriptExtender/Lua/Version.lua"
+# (meta.lsx)
+(Get-Content "$absPath/mod/Mods/AuridhDS/meta.lsx") `
+    -replace '((?<! ) {20}<attribute id="Version" type="int64" value=")(\d*)("\/>)',{ '{0}' -f $_.Groups[1].Value + ([long]$_.Groups[2].Value + 1) + $_.Groups[3].Value } `
+| Set-Content "$absPath/mod/Mods/AuridhDS/meta.lsx"
+
+# loop localization files
+$locaFiles = Get-ChildItem "$absPath/mod/Localization/" -Filter *.loca -File -Recurse
+foreach ($f in $locaFiles) {
+    divine -a convert-loca -g bg3 -s $f.FullName -d "$(join-path $f.DirectoryName $f.BaseName).loca"
+}
+# loop lsx files
+$lsxFiles = Get-ChildItem "$absPath/mod/Public" -Filter *.lsx -File -Recurse
+foreach ($f in $lsxFiles) {
+    divine -a convert-resource -g bg3 -s $f.FullName -d "$(join-path $f.DirectoryName $f.BaseName).lsf"
+}
+
+divine -a create-package -g bg3 -s "$absPath/mod" -d "$absPath/compile/files/$n.pak" -c lz4
 Compress-Archive -Path "$absPath/compile/files/$n.pak" -DestinationPath "$absPath/compile/files/$n.zip" -Force
