fac3b946e41968ae1b3ce05c5842c8d0bf8139fb
Auridh <git@auridh.net>
2023-10-05 15:09:59 +0200

    :fire: Remove unused configurations

--- compile/compile_addon.ps1
+++
@@ -1,9 +0,0 @@
-param ($n,$addon)
-$absPath = Resolve-Path -Path "./"
-divine -a convert-loca -g bg3 -s "$absPath/addons/$addon/Localization/English/$addon.xml" -d "$absPath/addons/$addon/Localization/English/$addon.loca";
-divine -a convert-resources -g bg3 -s "$absPath/addons/$addon/Public/$addon/RootTemplates" -d "$absPath/addons/$addon/Public/$addon/RootTemplates" -i lsx -o lsf;
-divine -a convert-resources -g bg3 -s "$absPath/addons/$addon/Public/$addon/Content/UI/[PAK]_UI" -d "$absPath/addons/$addon/Public/$addon/Content/UI/[PAK]_UI" -i lsx -o lsf;
-divine -a convert-resources -g bg3 -s "$absPath/addons/$addon/Public/$addon/GUI" -d "$absPath/addons/$addon/Public/$addon/GUI" -i lsx -o lsf;
-divine -a convert-resources -g bg3 -s "$absPath/addons/$addon/Public/$addon/Tags" -d "$absPath/addons/$addon/Public/$addon/Tags" -i lsx -o lsf;
-divine -a create-package -g bg3 -s "$absPath/addons/$addon" -d "$absPath/compile/files/$n.pak" -c lz4;
-Compress-Archive -Path "$absPath/compile/files/$n.pak" -DestinationPath "$absPath/compile/files/$n.zip" -Force
